e87810cc2b06da0f9c1519e71ebcc3ac6b81446c
Ахмед <@DESKTOP-KQ96IM8>
2021-12-10 22:48:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write ("Введите предложение:");
+           string s = Console.ReadLine();
+            string[] str = s.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int maxlen =0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > maxlen)
+                {
+                    maxlen = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово состоит из " + maxlen + " " + "букв");
+            Console.ReadKey();
+        }
+
+        
+    }
+   
+}
